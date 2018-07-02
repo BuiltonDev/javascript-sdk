b8488324e3474afd3199993cd933bf6d8e5c601c
Nicolas Bonduel <nicolas.bonduel@gmail.com>
2018-07-02 10:31:01 +0200

    added plans

---
+++ src/collection/plan.js
@@ -0,0 +1,24 @@
+const Components = require('./components');
+const {
+  create,
+  del,
+  get,
+  getAll,
+  refresh,
+  update,
+} = require('../utils/restFunctions');
+
+class Plan extends Components {
+  constructor(props) {
+    super(props);
+    this.apiPath = 'plans';
+    this.create = create.bind(this);
+    this.del = del.bind(this);
+    this.get = get.bind(this);
+    this.getAll = getAll.bind(this);
+    this.refresh = refresh.bind(this);
+    this.update = update.bind(this);
+  }
+}
+
+module.exports = Plan;
