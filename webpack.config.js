660c411debb0d2e4c9d2f4d7ec9fb8aed6a57a01
Nicolas Bonduel <nicolas.bonduel@gmail.com>
2018-10-08 17:43:09 +0200

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,9 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
     },
     ],
   },
