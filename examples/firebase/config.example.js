418644a2e71b633985b916660c3a9ae1a3502fb5
Nicolas Bonduel <nicolas.bonduel@gmail.com>
2018-10-18 16:05:34 +0200

    firebase authentication example

---
+++ examples/firebase/config.example.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: 'YOUR_kvass_API_KEY',
+  endpoint: 'http://qa.kvass.ai/',
+  firebase: {
+    apiKey: 'YOUR_FIREBASE_API_KEY',
+    authDomain: 'YOUR_FIREBASE_AUTH_DOMAIN',
+  },
+};
