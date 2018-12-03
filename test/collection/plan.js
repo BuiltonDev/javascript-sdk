49960e43c7e8cb143b5614edfd506f45cfd3b885
Nicolas Bonduel <nicolas.bonduel@gmail.com>
2018-12-03 14:35:50 +0100

    update tests

--- test/collection/plan.js
+++ test/collection/plan.js
@@ -15,7 +15,7 @@ describe('Plan related tests', () => {
   it('Should return all the plans', (done) => {
     url = `${endpoint}plans`;
     mock.get(url, () => ({ body: plansFile, ok: true }));
-    sa.plans().getAll({}, (err, plans) => {
+    sa.plan().getAll({}, (err, plans) => {
       assert.ok(Array.isArray(plans));
       assert.ok(plansFile[0]._id.$oid === plans[0].id);
       done();
