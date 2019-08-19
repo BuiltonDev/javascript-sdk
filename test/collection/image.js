0acde9bf18d6d98f39322f3bdb2c5b072e602281
Nicolas Bonduel <nicolas.bonduel@gmail.com>
2019-08-19 13:28:31 +0200

    fix pagination test

--- test/collection/image.js
+++ test/collection/image.js
@@ -1,26 +1,26 @@
 /* global it, describe, require */
 const assert = require('assert');
 const fs = require('fs');
-const Builton = require('../../src/main.js');
-
 const nock = require('nock');
+const Builton = require('../../src/main.js');
 
 const imageFile = require('../fetchmock/image.json');
 
 const endpoint = 'https://example.com';
 const bearerToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 const sa = new Builton({ apiKey: 'dummy', bearerToken, endpoint });
+const imageData = fs.createReadStream(`${__dirname}/../fetchmock/1x1.png`);
 
 describe('Image related tests', () => {
-  it.skip('Should upload an image', async () => {
+  it('Should upload an image', async () => {
     nock(endpoint)
       .post('/images')
       .reply(200, imageFile);
-    const imageData = fs.createReadStream('../fetchmock/1x1.png');
     try {
       const image = await sa.images.create({ imageData });
       assert.equal(image.original_name, '1x1.png');
     } catch (err) {
+      assert.fail(err);
       console.error(err);
     }
   });
