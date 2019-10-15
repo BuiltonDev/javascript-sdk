7acc5c14ef14ee142b046bcca4bffb162fc4497e
Sofien Rakke Lazreg <lazreg87@gmail.com>
2019-10-15 14:32:22 +0200

    Update payment.js

--- src/collection/objects/payment.js
+++ src/collection/objects/payment.js
@@ -15,7 +15,8 @@ class Payment extends Component {
       type: 'post', resource: 'pay', body, urlParams, json,
     }, done);
   }
-
+  
+  // Confirm the payment due to SCA triggered
   confirm(body, { urlParams, json = false } = {}, done) {
     return this.query({
       type: 'post', resource: 'confirm', body, urlParams, json,
